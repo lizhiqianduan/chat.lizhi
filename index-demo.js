426b885ea8683ccb348093c1c2b9ca43d3d7ca32
lizhiqianduan <1024669228@qq.com>
2015-05-05 18:03:47 +0800

    'it-can-work-ok'

--- index-demo.js
+++ index-demo.js
@@ -40,17 +40,56 @@ var clientList = [];
 
 // 客户端的连接事件
 socket.on("connection", function(client){
+  // push到客户端的连接数组里面
+  clientList.push(client);
 
-// 通过客户端的client.handshake对象可以获取客户端的信息，user-agent、cookie、address、
-  console.log('该客户端的client.conn.id：',client.conn.id,'client.handshake：',client.handshake);
+  // 用conn.id来辨别不同的客户端，即：一个连接一个客户端
+  var clientInfo = client.conn.id;
+  console.log(clientInfo);
+
+
+  // 通过客户端的client.handshake对象可以获取客户端的信息，user-agent、cookie、address、
+  // console.log('该客户端的client.conn.id：',client.conn.id,'client.handshake：',client.handshake);
+
 
   // 收到客户端的消息
   client.on("message",function(data){ 
-    console.log("收到客户端的消息：",data);
-    client.emit("emitMessage", { isSuccess:1,hello: "你好,服务器已经收到你的消息了："+data});
-  }); 
+    // console.log("收到客户端的消息：",data);
+    client.emit("emitMessage", { sendSuccess:1,message:data.message,username:data.username,connId:clientInfo});
+
+    // 记录socket不可写的客户端
+    var cleanList = [];
+
+    // 遍历客户端列表发消息
+    for (var i = 0; i < clientList.length; i++) {
+
+      // 如果某个客户端不可写，就将它的套接字关闭，并push进数组记录下来方便清除
+      // if (!clientList[i].writable) {
+      //   cleanList[i].destory();
+      //   cleanList.push(clientList[i]);
+      //   break;
+      // };
+
+      // 如果可写，那么发送数据，不需要给自身发数据
+      if (clientList[i].conn.id != clientInfo) {
+        // console.log(clientList[i].conn.id,clientInfo,clientList[i].conn.id != clientInfo);
+        clientList[i].emit("emitMessage", {otherClientData:1, message: data.message,username:data.username});
+      };
+
+    };
+
+    // 遍历结束后，再来清除掉所有不可写的socket客户端
+    // for (var i = 0; i < cleanList.length; i++) {
+    //   clientList.splice(clientList.indexOf(cleanList[i]),1);
+    // };
+
+  });
+
+  // 客户端断开连接事件 -->更新客户端列表
   client.on("disconnect",function(){ 
-    console.log("Server has disconnected");
+    // console.log("Server has disconnected");
+    clientList.splice(clientList.indexOf(client),1);
+
   }); 
   // client.send("hello, I am the server");
 });
